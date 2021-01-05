d596cd05cb942dbe0b0afad728cfb2c1388a8a1c
madiouni mohamed <madiounimohamed92@gmail.com>
2021-01-05 09:13:41 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import Navbar from './layout/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ paddingTop: 70 }}>
      <div className="App_center">
         <Navbar />
+        <div className="home-page">
+<img src="/sources/home_welcome.png" alt=""/>
+        </div>
 </div>
     </div>
   );
